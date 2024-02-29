e627bf12d9640843a75745badedf5f04164d5c83
Abdel-ilah Zakaria Khiati <zakria.ai@gmail.com>
2024-02-29 16:56:31 +0400

    Update IsPrime_example.java

--- Chapter 1-8/IsPrime_example.java
+++ Chapter 1-8/IsPrime_example.java
@@ -1,1 +1,22 @@
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(isPrime(121));
+        System.out.println(isPrime(8));
+        System.out.println(isPrime(5));
+    }
+  
+    public static boolean isPrime(int x){
+        if ((x==0) || (x==1)){
+            return false;
+        }
+        for(int i =2; i<x; i++){
+            if (x%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
